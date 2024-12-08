bcae84768e12f59923da4abfc3ba60efd7902298
Mateus Braga <teteu040506@gmail.com>
2024-12-08 20:40:09 -0300

    feat: my personal apps

--- 1.Setup.sh
+++ 1.Setup.sh
@@ -2,56 +2,11 @@
 
 # Setup Script
 
-# Uninstall Bloatware Apps
-sudo apt --purge remove -y yelp*
-sudo apt --purge remove -y gnome-logs
-sudo apt --purge remove -y seahorse
+# Uninstall some Bloatware Apps
 sudo apt --purge remove -y gnome-contacts
-sudo apt --purge remove -y geary
-sudo apt --purge remove -y libreoffice*
-sudo apt --purge remove -y gnome-weather
-sudo apt --purge remove -y ibus-mozc
-sudo apt --purge remove -y mozc-utils-gui
-sudo apt --purge remove -y gucharmap
-sudo apt --purge remove -y simple-scan
-sudo apt --purge remove -y popsicle
-sudo apt --purge remove -y popsicle-gtk
-sudo apt --purge remove -y totem*
-sudo apt --purge remove -y lm-sensors*
-sudo apt --purge remove -y xfburn
-sudo apt --purge remove -y xsane*
-sudo apt --purge remove -y hv3
-sudo apt --purge remove -y exfalso
-sudo apt --purge remove -y parole
-sudo apt --purge remove -y quodlibet
-sudo apt --purge remove -y xterm
-sudo apt --purge remove -y redshift*
-sudo apt --purge remove -y drawing
-sudo apt --purge remove -y hexchat*
-sudo apt --purge remove -y thunderbird*
-sudo apt --purge remove -y transmission*
-sudo apt --purge remove -y transmission-gtk*
-sudo apt --purge remove -y transmission-common*
-sudo apt --purge remove -y webapp-manager
-sudo apt --purge remove -y celluloid
-sudo apt --purge remove -y hypnotix
-sudo apt --purge remove -y rhythmbox*
-sudo apt --purge remove -y librhythmbox-core10*
-sudo apt --purge remove -y rhythmbox-data
-sudo apt --purge remove -y mintbackup
-sudo apt --purge remove -y mintreport
-sudo apt --purge remove -y aisleriot
 sudo apt --purge remove -y gnome-mahjongg
 sudo apt --purge remove -y gnome-mines
-sudo apt --purge remove -y quadrapassel
 sudo apt --purge remove -y gnome-sudoku
-sudo apt --purge remove -y cheese*
-sudo apt --purge remove -y pitivi
-sudo apt --purge remove -y gnome-sound-recorder
-sudo apt --purge remove -y remmina*
-sudo apt --purge remove -y gimp*
-sudo apt --purge remove -y zorin-windows-app-support-installation-shortcut
-sudo apt --purge remove -y firefox-esr
 sudo apt --purge remove -y gnome-todo
 
 # System Update and Upgrade
--- 2.Apps-to-Install.sh
+++ 2.Apps-to-Install.sh
@@ -1,233 +1,89 @@
 #!/bin/bash
 
-# Apps to Install Script
+# ----------------------------
+# Script de setup inicial para distros baseadas em Ubuntu
+#
+# aplicado em um Ubuntu 22.04 - dez/2024 
+# ----------------------------
 
-# System Update
-sudo apt update
+# Atualização do sistema
+update_system() {
+    echo "🔄 Atualizando o sistema..."
+    sudo apt update && sudo apt upgrade -y
+}
 
-# Apps
-sudo apt install -y gparted
-sudo apt install -y synaptic
-sudo apt install -y gufw
-sudo apt install -y hardinfo
-sudo apt install -y baobab
-sudo apt install -y htop
-sudo apt install -y net-tools
-sudo apt install -y ttf-mscorefonts-installer
-sudo apt install -y flameshot
-sudo apt install -y adb
-sudo apt install -y scrcpy
-sudo apt install -y vlc
-sudo apt install -y numlockx
-sudo apt install -y inetutils-traceroute
-sudo apt install -y tldr
-## If you are going to install Cryptomator, you might need to install this package:
-sudo apt install -y fuse3
-## If you are going to install VirtualBox, you might need to install this packages. But, in order for these packages to be installed on your PC, make sure that in the "Software & Updates" settings the "Download from" option is selected as "Main server". After that, make sure that you have enabled the Universe Repository; if not, you can do so by selecting the "Community-maintained free and open-source software (universe)" option. Finally, press the "Close" button.
-sudo apt install -y libqt5help5 libqt5opengl5 libxcb-cursor0
+# Instalação de pacotes essenciais
+install_essentials() {
+    echo "📦 Instalando pacotes essenciais..."
+    sudo apt install -y \
+        build-essential \
+        curl \
+        wget \
+        git \
+        vim \
+        htop \
+        net-tools \
+        zsh \
+        unzip \
+        gnupg \
+        software-properties-common \
+        gparted \
+        ttf-mscorefonts-installer \
+        vlc \
+        ffmpeg
+}
 
-## If your DE (Desktop Environment) is not GNOME, download the package below. Otherwise, use the KDE-Connect browser extension.
-sudo apt install -y kdeconnect
-## If you are going to install OBS Studio, download the packages below.
-sudo apt install -y v4l2loopback-dkms
-sudo apt install -y ffmpeg
-## If you want to change the XFCE Lock Screen Wallpaper, install this package:
-sudo apt install -y lightdm-gtk-greeter-settings
-## To use Java based applications like LibreOffice extensions and/or develop java apps, install the JRE or JDK packages below.
-## JRE/JDK 17 from OpenJDK
-### sudo apt install -y openjdk-17-jdk
-### sudo apt install -y openjdk-17-jdk-headless
-### sudo apt install -y openjdk-17-jre
-### sudo apt install -y openjdk-17-jre-headless
-### sudo apt install -y openjdk-17-jre-zero
-## JRE/JDK 21 from OpenJDK
-### sudo apt install -y openjdk-21-jdk
-### sudo apt install -y openjdk-21-jdk-headless
-### sudo apt install -y openjdk-21-jre
-### sudo apt install -y openjdk-21-jre-headless
-### sudo apt install -y openjdk-21-jre-zero
+# Instalação do Docker
+install_docker() {
+    echo "🐳 Instalando Docker..."
+    curl -fsSL https://get.docker.com | sh
+    sudo usermod -aG docker $USER
+}
 
-## If your DE is GNOME, then download the packages below.
-### sudo apt install -y gnome-tweaks
-### sudo apt install -y dconf-editor
+# Instalação do Docker Compose
+install_docker_compose() {
+    echo "🛠 Instalando Docker Compose..."
+    sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    sudo chmod +x /usr/local/bin/docker-compose
+}
 
-# Other apps:
-## sudo apt install -y clamav
-## sudo apt install -y clamav-daemon
-## sudo apt install -y libclamunrar9
-## sudo apt install -y clamtk
-## sudo apt install -y chkrootkit
-## sudo apt install -y rkhunter
+# Configuração do Git
+setup_git() {
+    read -p "📝 Digite seu nome para o Git: " git_name
+    read -p "📧 Digite seu e-mail para o Git: " git_email
 
-# PPA's Apps from Launchpad
-## Fastfetch
-sudo add-apt-repository ppa:zhangsongcui3371/fastfetch -y
-## Grub Customizer
-sudo add-apt-repository ppa:danielrichter2007/grub-customizer -y
-## Papirus Icons
-sudo add-apt-repository ppa:papirus/papirus -y
-## qBittorrent
-sudo add-apt-repository ppa:qbittorrent-team/qbittorrent-stable -y
-## Git
-sudo add-apt-repository ppa:git-core/ppa -y
-## Cryptomator
-sudo add-apt-repository ppa:sebastian-stenzel/cryptomator -y
-## KeePassXC
-sudo add-apt-repository ppa:phoerious/keepassxc -y
-## OBS Studio
-sudo add-apt-repository ppa:obsproject/obs-studio -y
-## Apache
-### sudo add-apt-repository ppa:ondrej/apache2 -y
-## PHP
-### sudo LC_ALL=en_US.UTF-8 add-apt-repository ppa:ondrej/php -y
-## Python 3.12.x
-### sudo add-apt-repository ppa:deadsnakes/ppa -y
+    git config --global user.name "$git_name"
+    git config --global user.email "$git_email"
 
-### System Update
-sudo apt update -qq
-#### Installing the Programs
-sudo apt install -y fastfetch
-sudo apt install -y grub-customizer
-sudo apt install -y papirus-icon-theme
-sudo apt install -y qbittorrent
-sudo apt install -y git
-sudo apt install -y cryptomator
-sudo apt install -y keepassxc
-sudo apt install -y obs-studio
-##### sudo apt install -y apache2
-##### sudo apt install -y php8.1
-##### sudo apt install -y python3.12 libpython3.12
+    echo "✅ Configuração do Git concluída."
+}
 
-# PPA's Apps from Other Sources
-## Node.js LTS (v20.10.x) - https://github.com/nodesource/distributions#using-ubuntu-1
-### curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash - &&\
-## Brave Browser
-sudo apt install apt-transport-https curl
-sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-## AnyDesk
-wget -qO - https://keys.anydesk.com/repos/DEB-GPG-KEY | apt-key add -
-echo "deb http://deb.anydesk.com/ all main" > /etc/apt/sources.list.d/anydesk-stable.list
-### System Update
-sudo apt update -qq
-### PPA's Apps from Other Sources installation
-sudo apt install -y nodejs
-sudo apt install -y brave-browser
-sudo apt install -y anydesk
+# Instalação do Oh My Zsh
+install_ohmyzsh() {
+    echo "💻 Instalando Oh My Zsh..."
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+}
 
-### If Node.js was installed, probably npm was installed along with it. So, consider updating npm to the latest version:
-#### sudo npm config set fund false --location=global
-#### sudo npm install -g npm@latest
+# Instalação de Node.js (via nvm)
+install_node() {
+    echo "🌐 Instalando Node.js via nvm..."
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+    source ~/.bashrc
+    nvm install --lts
+}
 
-# Flatpak Update
-flatpak update -y
+# ----------------------------
+# Execução das funções
+# ----------------------------
 
-# Flatpak Apps
-## Tor Browser
-flatpak install flathub com.github.micahflee.torbrowser-launcher -y
-## GIMP
-flatpak install flathub org.gimp.GIMP -y
-## Audacity
-flatpak install flathub org.audacityteam.Audacity -y
-## FileZilla
-flatpak install flathub org.filezillaproject.Filezilla -y
-## Okular
-flatpak install flathub org.kde.okular -y
-## Discord
-### flatpak install flathub com.discordapp.Discord -y
-## Zoom
-### flatpak install flathub us.zoom.Zoom -y
-## Microsoft Skype
-### flatpak install flathub com.skype.Client -y
-## Microsoft Teams
-### flatpak install flathub com.github.IsmaelMartinez.teams_for_linux -y
-## Todoist
-### flatpak install flathub com.todoist.Todoist -y
-## Slack
-### flatpak install flathub com.slack.Slack -y
-## Chromium
-### flatpak install flathub org.chromium.Chromium -y
+echo "🚀 Iniciando o setup inicial..."
 
-# Flatpak Clean Up
-flatpak uninstall --delete-data -y
-flatpak uninstall --unused -y
+update_system
+install_essentials
+install_docker
+install_docker_compose
+setup_git
+install_ohmyzsh
+install_node
 
-# Snap Update
-sudo snap refresh
-
-# Snap Apps
-## Eclipse IDE
-sudo snap install eclipse --classic
-
-# Snap Clean Up
-sudo rm -rf /var/lib/snapd/cache/*
-
-# wget Apps
-## BleachBit
-wget -c "https://download.bleachbit.org/bleachbit_4.6.0-0_all_ubuntu2204.deb"
-## bottom
-wget -c "https://github.com/ClementTsang/bottom/releases/download/0.10.2/bottom_0.10.2-1_amd64.deb"
-## GitHubDesktop
-wget -c "https://github.com/shiftkey/desktop/releases/download/release-3.4.3-linux1/GitHubDesktop-linux-amd64-3.4.3-linux1.deb"
-## GoogleChrome
-wget -c "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
-## LibreOffice
-wget -c "https://mirror.uepg.br/tdf/libreoffice/stable/24.8.0/deb/x86_64/LibreOffice_24.8.0_Linux_x86-64_deb.tar.gz"
-## LibreOffice LangPack for pt-BR
-wget -c "https://mirror.uepg.br/tdf/libreoffice/stable/24.8.0/deb/x86_64/LibreOffice_24.8.0_Linux_x86-64_deb_helppack_pt-BR.tar.gz"
-## VSCode
-wget -c "https://vscode.download.prss.microsoft.com/dbazure/download/stable/fee1edb8d6d72a0ddff41e5f71a671c23ed924b9/code_1.92.2-1723660989_amd64.deb"
-## VirtualBox & VirtualBox Extension Pack
-wget -c "https://download.virtualbox.org/virtualbox/7.1.4/virtualbox-7.1_7.1.4-165100~Ubuntu~jammy_amd64.deb"
-wget -c "https://download.virtualbox.org/virtualbox/7.1.4/Oracle_VirtualBox_Extension_Pack-7.1.4.vbox-extpack"
-## JDK - IBM Semuru Runtime Open Edition (Java 21 LTS)
-wget -c "https://objects.githubusercontent.com/github-production-release-asset-2e65be/695265943/7545f6af-79a1-42b3-b6dc-0aa57bfe981d?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=releaseassetproduction%2F20241118%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20241118T144040Z&X-Amz-Expires=300&X-Amz-Signature=fccf93a0e7a049a9c5a14111930e8455857cd8283c6c896e053d11b98b27aa1f&X-Amz-SignedHeaders=host&response-content-disposition=attachment%3B%20filename%3Dibm-semeru-open-jdk_x64_linux_21.0.4_7_openj9-0.46.1.tar.gz&response-content-type=application%2Foctet-stream"
-## JRE - IBM Semuru Runtime Open Edition (Java 21 LTS)
-wget -c "https://objects.githubusercontent.com/github-production-release-asset-2e65be/695265943/404b777e-5a3e-4324-a263-c651fc92d6ba?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=releaseassetproduction%2F20241118%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20241118T144242Z&X-Amz-Expires=300&X-Amz-Signature=cf4150f76411673366bfa3f6474aacb52fea64fe0c6119a91a00354c2ff02a6c&X-Amz-SignedHeaders=host&response-content-disposition=attachment%3B%20filename%3Dibm-semeru-open-jre_x64_linux_21.0.4_7_openj9-0.46.1.tar.gz&response-content-type=application%2Foctet-stream"
-## GitKraken
-### wget -c "https://release.gitkraken.com/linux/gitkraken-amd64.deb"
-## WordPress
-### wget -c "https://wordpress.org/latest.zip"
-## Local by Flywheel (WordPress Local Server)
-### wget -c "https://cdn.localwp.com/stable/latest/deb"
-## Angry IP Scanner
-### wget -c "https://github.com/angryip/ipscan/releases/download/3.9.1/ipscan_3.9.1_amd64.deb"
-
-### Installing the wget Programs
-sudo dpkg -i bleachbit_4.6.0-0_all_ubuntu2204.deb bottom_0.10.2-1_amd64.deb GitHubDesktop-linux-amd64-3.4.3-linux1.deb google-chrome-stable_current_amd64.deb code_1.92.2-1723660989_amd64.deb virtualbox-7.1_7.1.4-165100~Ubuntu~jammy_amd64.deb
-#### gitkraken-amd64.deb local-8.0.0-linux.deb ipscan_3.9.1_amd64.deb
-
-## AutoKey Installation
-wget -c "https://github.com/autokey/autokey/releases/download/v0.96.0/autokey-common_0.96.0_all.deb"
-wget -c "https://github.com/autokey/autokey/releases/download/v0.96.0/autokey-gtk_0.96.0_all.deb"
-sudo dpkg -i autokey-common_0.96.0_all.deb autokey-gtk_0.96.0_all.deb
-sudo apt --fix-broken install -y
-sudo dpkg -i autokey-common_0.96.0_all.deb autokey-gtk_0.96.0_all.deb
-
-# Some Other Settings for Gnome DE distros
-## Permanently enable Do Not Disturb (No Notifications)
-### gsettings set org.gnome.desktop.notifications show-banners false
-## Permanently disable Bluetooth
-### sudo systemctl disable bluetooth.service
-## Enable Hidden Startup Programs
-### sudo sed -i 's/NoDisplay=true/NoDisplay=false/g' /etc/xdg/autostart/*.desktop
-## Disable UTC and use Local Time in your Linux distro if necessary
-### timedatectl set-local-rtc 1 --adjust-system-clock
-
-# Finishing Things Up
-## System Update and Upgrade
-sudo apt update
-sudo apt install --fix-missing -y
-sudo apt upgrade --allow-downgrades -y
-sudo apt full-upgrade --allow-downgrades -y
-## System Clean Up
-sudo apt install -f
-sudo apt autoremove -y
-sudo apt autoclean
-sudo apt clean
-
-# End of Script
-
-# Display Installation Complete Message
-echo "All done :)" 
-echo "Restart your computer, clean up your system using BleachBit (if you want to) and then restart the computer again."
+echo "✅ Setup concluído! Reinicie a máquina para aplicar todas as alterações."
