6e0beb6624f0fd68a0a4b2031fe6aa60093dc6a0
Mateus Braga <teteu040506@gmail.com>
2024-12-08 23:44:54 -0300

    feat: add aliases

---
+++ personal-alias.sh
@@ -0,0 +1,16 @@
+# shell commands
+alias cls='clear' 
+
+
+# git commands
+alias gtst='git status'
+alias gtadd='git add .'
+alias gtb='git branch'
+alias gtch='git checkout'
+alias gtlg='git log --oneline --graph'
+
+alias gtco='git commit'
+alias gtm='git merge'
+alias gtpush='git push'
+alias gtpull='git pull'
+alias gtf='git fetch'
